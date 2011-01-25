ec4cf34e0d417762b159c893c6f6a3339f8b052e
Nao Iizuka <iizuka@kyu-mu.net>
2011-01-25 21:27:20 +0900

    write basic code and document

--- lib/Data/Versioned.pm
+++ lib/Data/Versioned.pm
@@ -1,29 +1,274 @@
 package Data::Versioned;
 
-use strict;
-use warnings;
-our $VERSION = '0.01';
-
-1;
-__END__
+=encoding utf-8
 
 =head1 NAME
 
-Data::Versioned -
+Data::Versioned - Maintain revision history of data
 
 =head1 SYNOPSIS
 
   use Data::Versioned;
 
+  my $data = Data::Versioned->new;
+
+  $data->set('name', 'Taro');  # revision raises up to 1
+  print "revision: " . $data->get_last_rev . "\n";  # prints 1
+
+  $data->set('name', 'Jiro');
+  print "revision: " . $data->get_last_rev . "\n";  # prints 2
+
+  print "latest: " . $data->get_last_data('name') . "\n";  # prints Jiro
+  print "rev 1: " . $data->get_rev_data('name', 1) . "\n"; # prints Taro
+  print "rev 2: " . $data->get_rev_data('name', 2) . "\n"; # prints Jiro
+
+  my @revs = $data->get_available_revs('name');  # @revs = (1, 2)
+
+  $data->delete_rev_older_than('name', 2);       # deletes revision < 2
+
+  @revs = $data->get_available_revs('name');     # @revs = (2)
+
 =head1 DESCRIPTION
 
-Data::Versioned is
+Data::Versioned is a hash reference with data versioning.
+It acts like an tiny key-value storage with history.
+Its data reside only on memory.
+
+=cut
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+use Carp;
+
+sub _dump {
+    my ($self) = @_;
+
+    use Data::Dumper;
+    print Dumper $self->{revdata};
+}
+
+sub _debug {
+    my ($self) = @_;
+
+    use Devel::Size qw/total_size/;
+    print "total_size: "  . total_size( $self->{revdata} ) . "\n";
+}
+
+=head1 METHODS
+
+=head2 new
+
+Returns new container.
+
+=cut
+
+sub new {
+    my ($class) = @_;
+
+    return bless {
+        revdata => {},
+    }, $class;
+}
+
+=head2 clear
+
+Clears all data.
+
+=cut
+
+sub clear {
+    my ($self) = @_;
+
+    $self->{revdata} = {};
+    return;
+}
+
+=head2 get_data_history
+
+Returns all data value.
+
+    my $values = $data->get_data_history('name');
+
+=cut
+
+sub get_data_history {
+    my ($self, $name) = @_;
+    croak "name parameter must be specified" unless defined $name;
+
+    my $struct = $self->{revdata}->{$name};
+    unless ($struct) {
+        return [];
+    }
+    return $struct->{data};
+}
+
+=head2 get_available_revs
+
+    my $revs = $data->get_available_revs('name');
+
+=cut
+
+sub get_available_revs {
+    my ($self, $name) = @_;
+    croak "name parameter must be specified" unless defined $name;
+
+    my $struct = $self->{revdata}->{$name};
+    unless ($struct) {
+        return [];
+    }
+    my $num = scalar @{ $struct->{data} };
+    my @revs;
+    if ( $num > 0 ) {
+        my $last_rev = $self->{revdata}->{$name}->{headrev} || 0;
+        @revs = ($last_rev-$num+1 .. $last_rev);
+    }
+    return \@revs;
+}
+
+=head2 get_rev_data
+
+    my $value = $data->get_rev_data('name', 3);  # 3 is the revision to get
+
+=cut
+
+sub get_rev_data {
+    my ($self, $name, $revision) = @_;
+    croak "name parameter must be specified" unless defined $name;
+    croak "revision parameter must be specified" unless defined $revision;
+
+    my $struct = $self->{revdata}->{$name};
+    unless ($struct) {
+        carp "get_rev_data($name, $revision): data is not initialized";
+        return;
+    }
+    my $arr = $struct->{data};
+    my $last_rev = $struct->{headrev} || 0;
+    my $index = $#$arr - ($last_rev - $revision);
+    if ($index < 0 || $index > $#$arr) {
+        carp "get_rev_data($name, $revision): revision is out of bounds";
+        return;
+    }
+    return $arr->[$index];
+}
+
+=head2 replace_rev_data
+
+Replace existing revision value.
+
+    $data->replace_rev_data('name', 3, 'value');
+
+=cut
+
+sub replace_rev_data {
+    my ($self, $name, $revision, $value) = @_;
+    croak "name parameter must be specified" unless defined $name;
+    croak "revision parameter must be specified" unless defined $revision;
+
+    my $struct = $self->{revdata}->{$name};
+    unless ($struct) {
+        croak "set_rev_data($name, $revision, $value): data is not initialized";
+    }
+    my $arr = $struct->{data};
+    my $last_rev = $struct->{headrev} || 0;
+    my $index = $#$arr - ($last_rev - $revision);
+    if ($index < 0 || $index > $#$arr) {
+        croak "set_rev_data($name, $revision, $value): revision is out of bounds";
+    }
+    $arr->[$index] = $value;
+}
+
+=head2 set
+
+Set the new value for the key.
+
+    $data->set('name', 'value');
+
+=cut
+
+sub set {
+    my ($self, $name, $value) = @_;
+    croak "name parameter must be specified" unless defined $name;
+
+    $self->{revdata}->{$name}->{headrev}++;
+    push(@{ $self->{revdata}->{$name}->{data} }, $value);
+    return;
+}
+
+=head2 get_last_data
+
+Returns the data which has been set most recently.
+
+    my $value = $data->get_last_data('name');
+
+=cut
+
+sub get_last_data {
+    my ($self, $name) = @_;
+    croak "name parameter must be specified" unless defined $name;
+
+    my $struct = $self->{revdata}->{$name};
+    unless ($struct) {
+        return;
+    }
+    return $struct->{data}->[-1];
+}
+
+=head2 get_last_rev
+
+Returns the latest revision.
+
+    my $rev = $data->get_last_rev('name');
+
+=cut
+
+sub get_last_rev {
+    my ($self, $name) = @_;
+    croak "name parameter must be specified" unless defined $name;
+
+    my $struct = $self->{revdata}->{$name};
+    unless ($struct) {
+        return 0;
+    }
+    return $struct->{headrev} || 0;
+}
+
+=head2 delete_rev_older_than
+
+Delete revisions that are older than specified value.
+
+    $data->delete_rev_older_than('name', 2);
+
+=cut
+
+sub delete_rev_older_than {
+    my ($self, $name, $least_revision) = @_;
+    croak "name parameter must be specified" unless defined $name;
+
+    my $struct = $self->{revdata}->{$name};
+    my $last_rev = $struct->{headrev} || 0;
+    my $remain = $last_rev - $least_revision + 1;
+    if ($remain < 0) {
+        $remain = 0;
+    }
+    my $removal = scalar @{ $struct->{data} } - $remain;
+    if ($removal > 0) {
+        splice(@{ $struct->{data} }, 0, $removal);
+    }
+    return;
+}
+
+1;
+__END__
+
+=over 4
+
+=back
 
 =head1 AUTHOR
 
-Nao Iizuka E<lt>iizuka@kyu-mu.netE<gt>
-
-=head1 SEE ALSO
+Nao Iizuka E<lt>iizuka@cpan.orgE<gt>
 
 =head1 LICENSE
 
---
+++ t/01_data.t
@@ -0,0 +1,112 @@
+#!/usr/bin/perl
+use strict;
+use Test::More;
+use Data::Versioned;
+use utf8;
+
+$SIG{__WARN__} = sub {};
+
+my $data = Data::Versioned->new;
+is $data->get_last_rev('a'), 0, 'head rev of a is 0';
+$data->set('a', 'c');
+is $data->get_last_rev('a'), 1, 'head rev of a is 1';
+$data->set('a', 'd');
+is $data->get_last_rev('a'), 2, 'head rev of a is 2';
+$data->set('a', 'e');
+is $data->get_last_rev('a'), 3, 'head rev of a is 3';
+is $data->get_last_rev('b'), 0, 'head rev of b is 0';
+$data->set('b', 'e');
+is $data->get_last_rev('a'), 3, 'head rev of a is 3';
+is $data->get_last_rev('b'), 1, 'head rev of b is 1';
+
+my $revs = $data->get_available_revs('a');
+is @$revs, 3, '3 revs available';
+is_deeply $data->get_data_history('a'), ['c', 'd', 'e'], 'get_data_history()';
+
+is $data->get_rev_data('a', 0), undef, 'rev 0 of a is undef';
+is $data->get_rev_data('a', 1), 'c', 'rev 1 of a is c';
+is $data->get_rev_data('a', 2), 'd', 'rev 2 of a is d';
+is $data->get_rev_data('a', 3), 'e', 'rev 3 of a is e';
+
+$data->replace_rev_data('a', 2, 'あいう');
+is $data->get_rev_data('a', 1), 'c', 'rev 1 of a is c';
+is $data->get_rev_data('a', 2), 'あいう', 'rev 2 of a is changed';
+is $data->get_rev_data('a', 3), 'e', 'rev 3 of a is e';
+is_deeply $data->get_data_history('a'), ['c', 'あいう', 'e'], 'get_data_history()';
+
+$data->replace_rev_data('a', 2, 'g');
+is $data->get_rev_data('a', 1), 'c', 'rev 1 of a is c';
+is $data->get_rev_data('a', 2), 'g', 'rev 2 of a is changed';
+is $data->get_rev_data('a', 3), 'e', 'rev 3 of a is e';
+is_deeply $data->get_data_history('a'), ['c', 'g', 'e'], 'get_data_history()';
+
+diag('large data');
+my $longstr = 'あXx' x 1000000;
+$data->replace_rev_data('a', 2, $longstr);
+is $data->get_rev_data('a', 2), $longstr, 'rev 2 of a is a long string';
+
+is $data->get_rev_data('a', 4), undef, 'rev 4 of a is undef';
+
+diag('bulk');
+$data->set('a', rand(10)) for 1..10;
+is $data->get_last_rev('a'), 13, 'head rev of a is 13';
+is @{$data->get_available_revs('a')}, 13, '13 revs available';
+is @{$data->get_data_history('a')}, 13, '13 items of history available';
+
+$data->set('a', rand(100)) for 1..100;
+is $data->get_last_rev('a'), 113, 'head rev of a is 113';
+$revs = $data->get_available_revs('a');
+is @$revs, 113, '113 revs available';
+is $revs->[0], 1, 'oldest available rev is 1';
+is $revs->[-1], 113, 'newest available rev is 113';
+is @{$data->get_data_history('a')}, 113, '113 items of history available';
+
+diag('delete < 114');
+$data->delete_rev_older_than('a', 114);
+
+$revs = $data->get_available_revs('a');
+is $data->get_last_rev('a'), 113, 'head rev of a is 113';
+is @{$data->get_available_revs('a')}, 0, '0 rev available';
+is @{$data->get_data_history('a')}, 0, '0 item of history available';
+
+$data->set('a', rand(1000)) for 1..30;
+is $data->get_last_rev('a'), 143, 'head rev of a is 143';
+$revs = $data->get_available_revs('a');
+is @$revs, 30, '30 revs available';
+is $revs->[0], 114, 'oldest available rev is 114';
+is $revs->[-1], 143, 'newest available rev is 143';
+is @{$data->get_data_history('a')}, 30, '30 items of history available';
+
+diag('delete < 129');
+$data->delete_rev_older_than('a', 129);
+$revs = $data->get_available_revs('a');
+is @$revs, 15, '15 revs available';
+is $revs->[0], 129, 'oldest available rev is 129';
+is $revs->[-1], 143, 'newest available rev is 143';
+is @{$data->get_data_history('a')}, 15, '15 items of history available';
+
+diag('delete < 130');
+$data->delete_rev_older_than('a', 130);
+$revs = $data->get_available_revs('a');
+is @$revs, 14, '14 revs available';
+is $revs->[0], 130, 'oldest available rev is 130';
+is $revs->[-1], 143, 'newest available rev is 143';
+is @{$data->get_data_history('a')}, 14, '14 items of history available';
+
+diag('clear');
+$data->clear;
+$revs = $data->get_available_revs('a');
+is @$revs, 0, '0 rev available';
+is @{$data->get_data_history('a')}, 0, '0 item of history available';
+is $data->get_rev_data('a', 5), undef, 'rev 5 of a is undef';
+eval {
+    $data->replace_rev_data('a', 4, 'dfg'); # fails
+    1;
+} or do {
+    pass 'replace_rev_data fails';
+};
+is $data->get_rev_data('a', 4), undef, 'rev 4 of c is undef';
+is @$revs, 0, '0 rev available';
+is @{$data->get_data_history('a')}, 0, '0 item of history available';
+
+done_testing;
